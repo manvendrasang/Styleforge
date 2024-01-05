95794aa8fab4c5b7771776ca85f373fda9e064a0
Manvendra Sang <manvendrasang@gmail.com>
2024-01-05 12:14:32 +0530

    Sidenav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Sidenav from './components/Sidenav'
 
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//   )
-// }
-
-export default App
+function App() {
+    return (
+      <div>
+        <Sidenav />
+       </div>
+    )
+  }
+  
+  export default App
